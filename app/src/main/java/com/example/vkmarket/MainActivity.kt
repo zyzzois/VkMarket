2697c0f64dee437b376f19b0b7f7865bc3e15435
Octane Silvio <ruslangibadullin70@gmail.com>
2024-03-07 01:15:45 +0500

    init

---
+++ app/src/main/java/com/example/vkmarket/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.vkmarket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
